2d76678373a23b5b93dd82eea71eb7de11d566e3
Taiizor <41683699+Taiizor@users.noreply.github.com>
2023-12-06 19:22:53 +0300

    Infrastructure Preparation 46

--- src/Taiizor.Starterkit/Extension/ThemeAuthenticator.cs
+++ src/Taiizor.Starterkit/Extension/ThemeAuthenticator.cs
@@ -6,9 +6,17 @@ namespace Taiizor.Starterkit.Extension
 
         public string CookieName { get; set; }
 
+        public string ProviderName { get; set; }
+
         public string CookieDomain { get; set; }
 
         public int CookieValueLength { get; set; }
+
+        public int ProviderKeyLength { get; set; }
+
+        public string ProviderDisplay { get; set; }
+
+        public string ProviderKeySplit { get; set; }
 
         public string CookieValueSplit { get; set; }
     }
