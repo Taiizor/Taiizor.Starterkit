328044e37b4cf47dcee31c0b91ebd5f57fae41c5
Taiizor <41683699+Taiizor@users.noreply.github.com>
2024-01-18 04:40:03 +0300

    Infrastructure Preparation 54

--- src/Taiizor.Starterkit/Interface/IJavascriptInterop.cs
+++ src/Taiizor.Starterkit/Interface/IJavascriptInterop.cs
@@ -3,6 +3,8 @@ namespace Taiizor.Starterkit.Interface
     // Core javascript interop interface
     public interface IJavascriptInterop
     {
+        ValueTask DisposeAsync();
+
         ValueTask InvokeVoidAsync(string identifier, params object[] args);
 
         ValueTask<T> InvokeAsync<T>(string identifier, params object[] args);
